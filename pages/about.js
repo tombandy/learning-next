b51417fd403e6e57d00b1970945ba4910087163d
Tom Bandy <hello@tombandy.com>
2023-11-18 01:29:07 +0000

    added all the tutorial files

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,23 @@
-import Link from 'next/link'
-
-export default function About() {
+export default function About(props) {
     return (
-        <>
-            <h1>About!</h1>
-            <p>Lorem ipsum stuff and things and bits etc.</p>
-            <Link href="/">Home</Link>
-        </>
+      <>
+        <h2>About Us</h2>
+        <p>I started this website after watching <a target="_blank" href="https://www.youtube.com/watch?v=qwhMyVVnmKM">this YouTube video</a> on NextJS</p>
+        <p>Lookup Next Layout for better way to do headers and footers</p>
+        <p>Welcome to this amazing about page. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro dolore officiis atque voluptas, quas, repellendus cum, magnam a alias unde reiciendis voluptates aliquam maxime laborum? Quae omnis eius impedit et?</p>
+        <p>I have {props.repoCount} public repos on GitHub.</p>
+      </>
     )
-}
+  }
+  
+  export async function getStaticProps() {
+    const response = await fetch("https://api.github.com/users/tombandy")
+    const data = await response.json()
+  
+    return {
+      props: {
+        repoCount: data.public_repos
+      },
+      revalidate: 10
+    }
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import Link from 'next/link'
-
-export default function Home() {
+export default function Home(props) {
     return (
-        <>
-            <h1>Welcome!</h1>
-            <p>Lorem ipsum stuff and things and bits etc.</p>
-            <Link href="/about">About</Link>
-        </>
+      <div>
+        <h2>Welcome to our homepage.</h2>
+        <p>This is the best home page in the world. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aspernatur illum architecto cumque recusandae fuga sequi necessitatibus deleniti repellat harum nobis, dolor veniam vero deserunt. Voluptatibus, ducimus deserunt. Recusandae, dolore.</p>
+        <p>The weather: {props.forecast} using serversideprops, client side JS would be better but his is there for SEO!</p>
+      </div>
     )
-}
+  }
+  
+  export async function getServerSideProps() {
+    const response = await fetch("https://api.weather.gov/gridpoints/MFL/109,49/forecast")
+    const data = await response.json()
+  
+    return {
+      props: {
+        forecast: data.properties.periods[0].detailedForecast
+      }
+    }
+  }
