44e5649a36f90cc14d3806ad7a7dcefb25203f75
Tom Bandy <hello@tombandy.com>
2024-09-07 11:00:42 +0100

    updated footer text and colour to aqua

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,8 @@ import styles from "../styles/footer.module.css"
 const Footer = () => {
     return (
         <footer>
-            <p className={styles.p}>Copyright &copy; 2021</p>
-            <Link href="/about">Footer About</Link>
+            <p className={styles.p}>Copyright &copy; 2024</p>
+            <Link href="/about">Footer About v3</Link>
         </footer>
     )
 }
